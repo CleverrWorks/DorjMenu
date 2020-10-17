b15f7a0d8d1a0fff15c2b681312c25e5c32c51e4
Taosif7 <Taosif.7@gmail.com>
2020-10-17 22:02:14 +0530

    Initial commit

---
+++ app/src/main/java/com/taosif7/android/sidedrawermenu/MainActivity.java
@@ -0,0 +1,14 @@
+package com.taosif7.android.sidedrawermenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
