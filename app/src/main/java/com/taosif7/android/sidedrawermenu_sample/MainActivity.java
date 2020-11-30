f1b5b8d68e9e78d643aafc72061b314b8a0ab9b1
Taosif7 <taosif.7@gmail.com>
2020-12-01 01:20:11 +0530

    Added Controls to tweak drawer on the go

--- app/src/main/java/com/taosif7/android/sidedrawermenu_sample/MainActivity.java
+++ app/src/main/java/com/taosif7/android/sidedrawermenu_sample/MainActivity.java
@@ -1,17 +1,22 @@
 package com.taosif7.android.sidedrawermenu_sample;
 
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.taosif7.android.sidedrawermenu.SideDrawerMenu;
 import com.taosif7.android.sidedrawermenu.helpers.DrawerCallbacks;
 import com.taosif7.android.sidedrawermenu.models.menuItem;
@@ -21,7 +26,18 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements DrawerCallbacks {
 
-    private SideDrawerMenu menu;
+    private SideDrawerMenu drawerMenu;
+    private SharedPreferences sharedPreferences;
+
+    // properties
+    boolean ltr = false;
+    int menu_direction = 1;
+    int menu_type = 0;
+
+    // Constants
+    final String PROP_LTR = "left_to_right";
+    final String PROP_DIRECTION = "direction";
+    final String PROP_MENUTYPE = "menu_type";
 
     @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
@@ -29,8 +45,124 @@ public class MainActivity extends AppCompatActivity implements DrawerCallbacks {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<menuItem> items = new ArrayList<>();
+        // Get properties from shared Prefs
+        sharedPreferences = getSharedPreferences("drawer_lib", MODE_PRIVATE);
+        ltr = sharedPreferences.getBoolean(PROP_LTR, ltr);
+        menu_direction = sharedPreferences.getInt(PROP_DIRECTION, menu_direction);
+        menu_type = sharedPreferences.getInt(PROP_MENUTYPE, menu_type);
 
+        // Set Menu properties
+        drawerMenu = new SideDrawerMenu(this, this);
+        drawerMenu.setItems(getMenuItems());
+        drawerMenu.setMenuType((menu_type == 0) ? SideDrawerMenu.MenuType.MENU_SUBLIST : SideDrawerMenu.MenuType.MENU_PAGES);
+        drawerMenu.setHeaderBackground(getResources().getDrawable(R.drawable.drawer_bg));
+        drawerMenu.setUserDetails(getDrawable(R.drawable.profile_pic), "Marques Brownlee", "business@MKBHD.com", true);
+        drawerMenu.setPersistentButton(null, null, view -> {
+            Snackbar.make(getWindow().getDecorView(), "you can set custom Text, Icon and click listener to this button. you can even hide it.", Snackbar.LENGTH_SHORT).show();
+            drawerMenu.closeMenu();
+        });
+        drawerMenu.setCTAButton("Buy Pro version", ContextCompat.getColor(this, android.R.color.holo_green_dark), view -> Snackbar.make(getWindow().getDecorView(), "you can set custom Text and click listener to this button. you can even hide it.", Snackbar.LENGTH_SHORT).show());
+        drawerMenu.setHeaderButton(getDrawable(R.drawable.ic_baseline_edit_24), view -> Snackbar.make(getWindow().getDecorView(), "You can set and Icon and action to this button. you can even hide it.", Snackbar.LENGTH_SHORT).show());
+        drawerMenu.forceRTLLayout(ltr);
+        drawerMenu.attachToActivity(this, (menu_direction == 0) ? SideDrawerMenu.direction.LEFT : SideDrawerMenu.direction.RIGHT);
+
+        // Set control properties
+        ((SwitchCompat) findViewById(R.id.rtl_switch)).setChecked(ltr);
+        ((Spinner) findViewById(R.id.menu_direction_spinner)).setSelection(menu_direction);
+        ((Spinner) findViewById(R.id.menu_type_spinner)).setSelection(menu_type);
+
+        // Set listeners for controls
+
+        findViewById(R.id.toggle_btn).setOnClickListener(view -> drawerMenu.toggleMenu());
+
+        ((SwitchCompat) findViewById(R.id.rtl_switch)).setOnCheckedChangeListener((compoundButton, b) -> {
+            sharedPreferences.edit().putBoolean(PROP_LTR, b).apply();
+            restartActivity();
+        });
+
+        ((Spinner) findViewById(R.id.menu_direction_spinner)).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == menu_direction) return;
+                sharedPreferences.edit().putInt(PROP_DIRECTION, i).apply();
+                restartActivity();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        ((Spinner) findViewById(R.id.menu_type_spinner)).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == menu_type) return;
+                sharedPreferences.edit().putInt(PROP_MENUTYPE, i).apply();
+                restartActivity();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.toggle_menu) {
+            drawerMenu.toggleMenu();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onDrawerMenuItemClick(menuItem menuItem) {
+        if (menuItem.getLabel().equals("Travel")) return false;
+        ((TextView) findViewById(R.id.selectedItemText)).setText(menuItem.getLabel() + " is Selected");
+
+        drawerMenu.closeMenu();
+
+        menuItem parent = menuItem.getParent();
+        if (parent != null) {
+            switch (parent.getLabel()) {
+                case "Phones":
+                    if (menuItem.getLabel().contains("Iphone"))
+                        drawerMenu.setItemHighlightColor(ContextCompat.getColor(this, android.R.color.holo_purple));
+                    else
+                        drawerMenu.setItemHighlightColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark));
+                    break;
+                case "Tablets":
+                    drawerMenu.setItemHighlightColor(ContextCompat.getColor(this, android.R.color.holo_green_dark));
+                    break;
+                case "Watches":
+                    drawerMenu.setItemHighlightColor(ContextCompat.getColor(this, android.R.color.holo_blue_dark));
+                    break;
+                default:
+                    drawerMenu.setItemHighlightColor(ContextCompat.getColor(this, android.R.color.holo_blue_bright));
+            }
+        }
+
+        return true;
+    }
+
+    void restartActivity() {
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(getIntent());
+        overridePendingTransition(0, 0);
+        //recreate();
+    }
+
+    List<menuItem> getMenuItems() {
+        List<menuItem> items = new ArrayList<>();
         menuItem devicesMenu = new menuItem("Devices", ContextCompat.getDrawable(this, R.drawable.ic_baseline_devices_other_24));
         menuItem phonesItems = new menuItem("Phones", ContextCompat.getDrawable(this, R.drawable.ic_baseline_smartphone_24));
         menuItem tabletsItems = new menuItem("Tablets", ContextCompat.getDrawable(this, R.drawable.ic_baseline_tablet_android_24));
@@ -64,63 +196,6 @@ public class MainActivity extends AppCompatActivity implements DrawerCallbacks {
 
         items.add(devicesMenu);
         items.add(new menuItem("Travel", ContextCompat.getDrawable(this, R.drawable.ic_baseline_business_center_24)));
-
-        menu = new SideDrawerMenu(this, this);
-        menu.setItems(items);
-        menu.setMenuType(SideDrawerMenu.MenuType.MENU_SUBLIST);
-        menu.setHeaderBackground(getResources().getDrawable(R.drawable.wwe));
-        menu.setUserDetails(getDrawable(R.drawable.jhon), "Jhon Cena", "YouCantSeeMe@wwe.com", true);
-        menu.setPersistentButton(null, null, view -> {
-            Toast.makeText(MainActivity.this, "Settings activity will open up!", Toast.LENGTH_SHORT).show();
-            menu.closeMenu();
-        });
-        menu.setCTAButton("Buy Pro version", ContextCompat.getColor(this, android.R.color.holo_green_dark), view -> Toast.makeText(MainActivity.this, "Going into new activity", Toast.LENGTH_SHORT).show());
-        menu.setHeaderButton(getDrawable(R.drawable.ic_baseline_edit_24), view -> Toast.makeText(this, "Edit Profile", Toast.LENGTH_SHORT).show());
-        menu.attachToActivity(this, SideDrawerMenu.direction.RIGHT);
-        findViewById(R.id.toggle_btn).setOnClickListener(view -> menu.toggleMenu());
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.toggle_menu) {
-            menu.toggleMenu();
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onDrawerMenuItemClick(menuItem menuItem) {
-        if (menuItem.getLabel().equals("Travel")) return false;
-        ((TextView) findViewById(R.id.selectedItemText)).setText(menuItem.getLabel() + " is Selected");
-
-        menu.closeMenu();
-
-        menuItem parent = menuItem.getParent();
-        if (parent != null) {
-            switch (parent.getLabel()) {
-                case "Phones":
-                    if (menuItem.getLabel().contains("Iphone"))
-                        menu.setItemHighlightColor(ContextCompat.getColor(this, android.R.color.holo_purple));
-                    else
-                        menu.setItemHighlightColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark));
-                    break;
-                case "Tablets":
-                    menu.setItemHighlightColor(ContextCompat.getColor(this, android.R.color.holo_green_dark));
-                    break;
-                case "Watches":
-                    menu.setItemHighlightColor(ContextCompat.getColor(this, android.R.color.holo_blue_dark));
-                    break;
-                default:
-                    menu.setItemHighlightColor(ContextCompat.getColor(this, android.R.color.holo_blue_bright));
-            }
-        }
-
-        return true;
+        return items;
     }
 }
