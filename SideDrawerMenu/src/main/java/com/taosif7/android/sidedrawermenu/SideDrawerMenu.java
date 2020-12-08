eae230ddf158f46280be7e648e2dc14e103766b5
Taosif7 <taosif.7@gmail.com>
2020-12-08 12:45:30 +0530

    Added Spaced for android bottom navigation, because persistent button got hid under that

--- SideDrawerMenu/src/main/java/com/taosif7/android/sidedrawermenu/SideDrawerMenu.java
+++ SideDrawerMenu/src/main/java/com/taosif7/android/sidedrawermenu/SideDrawerMenu.java
@@ -14,6 +14,7 @@ import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
@@ -114,6 +115,9 @@ public class SideDrawerMenu extends LinearLayout {
 
         // Set User details
         setUserDetails(profileImage, displayName, email, true);
+
+        // Set bottom navigation bar spacer
+        findViewById(R.id.bottom_spacer).getLayoutParams().height = getNavigationBarHeight();
 
         // set persistent button & cta button props
         setPersistentButton(persistentButtonIcon, persistentButtonLabel, persistentButtonListener);
@@ -463,5 +467,14 @@ public class SideDrawerMenu extends LinearLayout {
     public void toggleMenu() {
         if (menu_open) closeMenu();
         else openMenu();
+    }
+
+    public int getNavigationBarHeight() {
+        boolean hasMenuKey = ViewConfiguration.get(getContext()).hasPermanentMenuKey();
+        int resourceId = getResources().getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceId > 0 && !hasMenuKey) {
+            return getResources().getDimensionPixelSize(resourceId);
+        }
+        return 0;
     }
 }
